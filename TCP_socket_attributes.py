a49472850f2317e1faa83ffb9269bc1ce945b8c7
chadys <chadys@hotmail.fr>
2018-12-14 17:15:27 +0100

    split volume and direction transfer in two sockets

--- TCP_socket_attributes.py
+++ TCP_socket_attributes.py
@@ -6,8 +6,12 @@ master_client_attributes = {
 }
 
 webapp_server_attributes = {
-    'TCP_IP': '',
-    'TCP_PORT': 6543,
+    'TCP_IP_VOLUME': '',
+    'TCP_PORT_VOLUME': 6542,
+    'WS_VOLUME': 8765,
+    'TCP_IP_DIRECTION': '',
+    'TCP_PORT_DIRECTION': 6543,
+    'WS_DIRECTION': 8766
 }
 
 leap_server_attributes = {
--- VolumeAndGyroWebApp/Web2TCPSocket.py
+++ VolumeAndGyroWebApp/Web2TCPSocket.py
@@ -3,22 +3,21 @@ import websockets
 import json
 import TCP_socket_attributes as tcpAttr
 
-
-async def retransfer(websocket, _):
+async def retransfer(websocket, _, isVolume):
     try:
         async for message in websocket:
             data = json.loads(message)
             print(message)
-            broadcast(data)
+            broadcast(data, isVolume)
 
     except websockets.exceptions.ConnectionClosed:
         pass
 
 
-async def send_to_subscribers(reader, writer):
+async def send_to_subscribers(reader, writer, isVolume):
     while not reader.at_eof():
         read_task = loop.create_task(reader.read(100))
-        for f in asyncio.as_completed({read_task, broadcast_data}):
+        for f in asyncio.as_completed({read_task, broadcast_data_volume if isVolume else broadcast_data_direction}):
             data = await f
             if isinstance(data, dict):
                 writer.write(data.popitem()[1].encode())
@@ -34,30 +33,51 @@ async def send_to_subscribers(reader, writer):
     writer.close()
 
 
-def broadcast(data):
-    global broadcast_data
-    broadcast_data.set_result(data)
-    broadcast_data = loop.create_future()
+def broadcast(data, isVolume):
+    global broadcast_data_volume, broadcast_data_direction
+    if isVolume:
+        broadcast_data_volume.set_result(data)
+        broadcast_data_volume = loop.create_future()
+    else:
+        broadcast_data_direction.set_result(data)
+        broadcast_data_direction = loop.create_future()
 
 
 loop = asyncio.get_event_loop()
-broadcast_data = loop.create_future()
-websocket_server = websockets.serve(retransfer, '', 8765, loop=loop)
-tcpsocket_server = asyncio.start_server(send_to_subscribers,
-                                        tcpAttr.webapp_server_attributes['TCP_IP'],
-                                        tcpAttr.webapp_server_attributes['TCP_PORT'],
-                                        loop=loop)
-websocket_server, tcpsocket_server = loop.run_until_complete(asyncio.gather(
-    websocket_server,
-    tcpsocket_server
-))
+
+broadcast_data_volume = loop.create_future()
+broadcast_data_direction = loop.create_future()
+
+websocket_server_volume = websockets.serve(lambda w, p: retransfer(w, p, True),
+                                           '', tcpAttr.webapp_server_attributes['WS_VOLUME'], loop=loop)
+websocket_server_direction = websockets.serve(lambda w, p: retransfer(w, p, False),
+                                              '', tcpAttr.webapp_server_attributes['WS_DIRECTION'], loop=loop)
+
+tcpsocket_server_volume = asyncio.start_server(lambda r, w: send_to_subscribers(r, w, True),
+                                               tcpAttr.webapp_server_attributes['TCP_IP_VOLUME'],
+                                               tcpAttr.webapp_server_attributes['TCP_PORT_VOLUME'],
+                                               loop=loop)
+tcpsocket_server_direction = asyncio.start_server(lambda r, w: send_to_subscribers(r, w, False),
+                                                  tcpAttr.webapp_server_attributes['TCP_IP_DIRECTION'],
+                                                  tcpAttr.webapp_server_attributes['TCP_PORT_DIRECTION'],
+                                                  loop=loop)
+
+tasks = websocket_server_volume,\
+        websocket_server_direction,\
+        tcpsocket_server_volume,\
+        tcpsocket_server_direction
+
+tasks = loop.run_until_complete(asyncio.gather(*tasks))
+
 try:
     loop.run_forever()
 except KeyboardInterrupt:
     pass
 finally:
-    broadcast_data.cancel()
-    tcpsocket_server.close()
-    loop.run_until_complete(tcpsocket_server.wait_closed())
+    tcpsocket_server_volume.close()
+    tcpsocket_server_direction.close()
+    loop.run_until_complete(asyncio.gather(tasks[2].wait_closed(), tasks[3].wait_closed()))
+    broadcast_data_volume.cancel()
+    broadcast_data_direction.cancel()
     asyncio.gather(*asyncio.Task.all_tasks()).cancel()
     loop.call_soon(loop.close)
