1475e587687a31f5931495ab3e89bcdb16890f0b
chadys <chadys@hotmail.fr>
2018-12-28 00:10:54 +0100

    make output discovery dynamic, fist try at directional volume function

--- DirectionalSpeakersApp/DirectionalSpeakerLaunch.py
+++ DirectionalSpeakersApp/DirectionalSpeakerLaunch.py
@@ -28,17 +28,21 @@ def print_infos():
         print(f'Device {device}')
         print(f'description = {bd.name}')
         print(f'driver = {bd.driver}')
-        print(f'flags = {bd.flags}')
+        print(f'flags = {hex(bd.flags)}')
         device += 1
     print(f'device selected = {BASS_GetDevice()}')
 
 
 def init_channels(mixer, handles):
     sides = [BASS_SPEAKER_LEFT, BASS_SPEAKER_RIGHT]
-    for i in range(2):
+    i = 0
+    while True:
         for j in range(2):
-            if not BASS_Mixer_StreamAddChannel(mixer, handles[i * 2 + j], BASS_SPEAKER_N(i + 1) | sides[j]):
-                handle_bass_error(getframeinfo(currentframe()).lineno)
+            if not BASS_Mixer_StreamAddChannel(mixer, handles[-1], BASS_SPEAKER_N(i + 1) | sides[j]):
+                handles.pop()
+                return
+            handles.append(get_stream())
+        i += 1
     #
     # if not BASS_Mixer_StreamAddChannel(mixer, handle, BASS_MIXER_MATRIX):
     #     handle_bass_error(getframeinfo(currentframe()).lineno)
@@ -55,38 +59,64 @@ def play(mixer, handles):
     BASS_ChannelPlay(mixer, False)
 
 
-def create_mixer(handles):
+def create_mixer(handle):
     bc = BASS_CHANNELINFO()
-    if not BASS_ChannelGetInfo(handles[0], bc):
+    if not BASS_ChannelGetInfo(handle, bc):
         handle_bass_error(getframeinfo(currentframe()).lineno)
     mixer = BASS_Mixer_StreamCreate(bc.freq, 4, 0)
     if not mixer:
         handle_bass_error(getframeinfo(currentframe()).lineno)
     return mixer
 
+
+def get_speaker_volume(direction, speaker_pos, global_volume = 1.0):
+    angle = abs(direction - speaker_pos)
+    if angle > 180:
+        angle = 360 - angle
+    angle /= 360  # get value between 0 and 1
+    angle = 1 - angle  # invert value
+    volume = pow(angle, 5)*(2*angle-0.5)
+    volume = 0 if volume < 0 else 1 if volume > 1 else volume
+    return volume * global_volume
+
+
+def init_pos(num_output):
+    pos = []
+    part = 360 / num_output
+    for i in range(num_output):
+        pos.append(i*part)
+    return pos
+
+
 def main():
     if not BASS_Init(-1, 44100, BASS_DEVICE_SPEAKERS | BASS_DEVICE_MONO, 0, 0):
         handle_bass_error(getframeinfo(currentframe()).lineno)
     print_infos()
 
-    handles = [get_stream(), get_stream(), get_stream(), get_stream()]
-    if not all(handles):
+    handles = [get_stream()]
+    if not handles[0]:
         handle_bass_error(getframeinfo(currentframe()).lineno)
 
-    mixer = create_mixer(handles)
+    mixer = create_mixer(handles[0])
     init_channels(mixer, handles)
+    print(f'Current device has {len(handles)} outputs')
+    pos = init_pos(len(handles))
     play(mixer, handles)
+    # for handle in handles:
+    #     if not BASS_ChannelSetAttribute(handle, BASS_ATTRIB_FREQ, 300000):
+    #         handle_bass_error(getframeinfo(currentframe()).lineno)
 
     channel_length = BASS_ChannelGetLength(handles[0], BASS_POS_BYTE)
     print(f'Music lenght : {BASS_ChannelBytes2Seconds(handles[0], channel_length)} sec')
 
     while BASS_ChannelIsActive(mixer) == BASS_ACTIVE_PLAYING:
         channel_position = BASS_ChannelGetPosition(handles[0], BASS_POS_BYTE)
-        print(int(BASS_ChannelBytes2Seconds(handles[0], channel_position)))
-        # if not BASS_ChannelSetAttribute(handles[0], BASS_ATTRIB_VOL, 0.1):
-        #     handle_bass_error(getframeinfo(currentframe()).lineno)
-        # if not BASS_ChannelSetAttribute(handles[2], BASS_ATTRIB_FREQ, 10000):
-        #     handle_bass_error(getframeinfo(currentframe()).lineno)
+        seconds_counter = int(BASS_ChannelBytes2Seconds(handles[0], channel_position))
+        print(seconds_counter)
+        direction = seconds_counter % 360
+        for index, handle in enumerate(handles):
+            if not BASS_ChannelSetAttribute(handle, BASS_ATTRIB_VOL, get_speaker_volume(direction, pos[index], 0.6)):
+                handle_bass_error(getframeinfo(currentframe()).lineno)
         time.sleep(2)
     if not BASS_Free():
         handle_bass_error(getframeinfo(currentframe()).lineno)
